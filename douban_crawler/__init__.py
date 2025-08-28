741080c9be5048e8490399c59f3e0f287ca86892
Jiang Chen <213230191@seu.edu.cn>
2025-08-28 23:44:25 +0800

    crawler_ver0.1

---
+++ douban_crawler/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+豆瓣电影爬虫模块
+用于大数据真值推荐系统项目的数据采集
+"""
+
+from .crawler import DoubanMovieCrawler
+from .data_processor import DataProcessor
+from .config import Config
+
+__version__ = "1.0.0"
+__author__ = "Jiang Chen"
+
+__all__ = [
+    'DoubanMovieCrawler',
+    'DataProcessor', 
+    'Config'
+]
